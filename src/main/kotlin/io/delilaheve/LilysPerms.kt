954fcfbcd934ff4e9fc242d5a70369f1a413b2d9
DelilahEve <pvt.caboose722@gmail.com>
2022-08-27 14:33:16 -0400

    Initial commit

---
+++ src/main/kotlin/io/delilaheve/LilysPerms.kt
@@ -0,0 +1,15 @@
+package io.delilaheve
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LilysPerms : JavaPlugin() {
+
+    override fun onEnable() {
+        // ToDo
+    }
+
+    override fun onDisable() {
+        // ToDo
+    }
+
+}
