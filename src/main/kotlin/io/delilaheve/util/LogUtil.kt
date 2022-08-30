e87bc9a12d100d7895735cdef8614d8c31133b03
DelilahEve <pvt.caboose722@gmail.com>
2022-08-30 08:53:15 -0400

    log util comments

--- src/main/kotlin/io/delilaheve/util/LogUtil.kt
+++ src/main/kotlin/io/delilaheve/util/LogUtil.kt
@@ -2,17 +2,14 @@ package io.delilaheve.util
 
 import io.delilaheve.LilysPermissions
 
+/**
+ * Utilities to ease logging
+ */
 object LogUtil {
 
-    private const val debugBuild = true
-
-    fun info(message: String) {
-        if (!debugBuild) { return }
-        LilysPermissions.instance
-            ?.logger
-            ?.info(message)
-    }
-
+    /**
+     * Log a warning [message]
+     */
     fun warn(message: String) {
         LilysPermissions.instance
             ?.logger
