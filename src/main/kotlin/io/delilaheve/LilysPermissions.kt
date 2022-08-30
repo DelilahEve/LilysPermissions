808115961e62a6bfd71864723e75f42eeb494dc3
DelilahEve <pvt.caboose722@gmail.com>
2022-08-30 10:01:04 -0400

    startup check to warn of potential configuration issues

--- src/main/kotlin/io/delilaheve/LilysPermissions.kt
+++ src/main/kotlin/io/delilaheve/LilysPermissions.kt
@@ -4,6 +4,10 @@ import io.delilaheve.handler.PlayerJoinHandler
 import io.delilaheve.handler.PlayerLeaveHandler
 import io.delilaheve.manager.CommandManager
 import io.delilaheve.manager.PlayerManager
+import io.delilaheve.util.GroupUtil
+import io.delilaheve.util.GroupUtil.DEFAULT_GLOBAL_KEY
+import io.delilaheve.util.LogUtil
+import io.delilaheve.util.YamlUtil
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
@@ -19,6 +23,10 @@ class LilysPermissions : JavaPlugin() {
         const val PERMISSIONS_FILE = "permissions.yml"
         const val USERS_FILE = "users.yml"
 
+        // Warning for when a configuration might be in error, but the plugin can still function in some capacity
+        private const val WARNING_NOT_INTENDED: String
+            = "If this is not intended you should check your $PERMISSIONS_FILE formatting."
+
         // Plugin instance
         var instance: LilysPermissions? = null
             private set
@@ -31,6 +39,7 @@ class LilysPermissions : JavaPlugin() {
         instance = this
         saveDefaults()
         registerHandlers()
+        startupCheck()
         // If there are already players online when we enable, we should be attaching to them
         if (server.onlinePlayers.isNotEmpty()) {
             PlayerManager.attachAllOnline()
@@ -77,6 +86,40 @@ class LilysPermissions : JavaPlugin() {
         ).forEach { server.pluginManager.registerEvents(it, this) }
     }
 
-
+    /**
+     * Check configurations for # of groups defined, # of users known,
+     * and validate at least on default group exists
+     */
+    private fun startupCheck() {
+        // Check the # of groups defined and log a warning if none are found
+        val groups = GroupUtil.allGroups()
+        if (groups.isEmpty()) {
+            LogUtil.warn("No permissions groups are defined. $WARNING_NOT_INTENDED")
+        } else {
+            LogUtil.info("${groups.size} groups defined")
+        }
+        // Check the # of default groups and whether a global default is defined
+        val defaultGroups = GroupUtil.allDefaults()
+        val haveGlobalDefault = defaultGroups.any {
+            it.default.equals(DEFAULT_GLOBAL_KEY, true)
+        }
+        when {
+            defaultGroups.isEmpty() -> LogUtil.warn("No default group has been defined. $WARNING_NOT_INTENDED")
+            !haveGlobalDefault -> LogUtil.warn("No global default group has been defined. $WARNING_NOT_INTENDED")
+            else -> {
+                val defaultsString = defaultGroups.joinToString(", ") {
+                    val forWorld = if (it.default.equals(DEFAULT_GLOBAL_KEY, true)) {
+                        "global"
+                    } else {
+                        it.default
+                    }
+                    "${it.name}: $forWorld"
+                }
+                LogUtil.info("${defaultGroups.size} default groups defined ($defaultsString)")
+            }
+        }
+        // loading the users file will log a warning if there are errors, we don't need to log anything here
+        YamlUtil.getFile(USERS_FILE)
+    }
 
 }
