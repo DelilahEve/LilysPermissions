1f75ad7a4a0e06905ded99c6d2c73fe2cac0eecc
DelilahEve <pvt.caboose722@gmail.com>
2022-08-30 08:51:14 -0400

    code comments

--- src/main/kotlin/io/delilaheve/util/ColourUtil.kt
+++ src/main/kotlin/io/delilaheve/util/ColourUtil.kt
@@ -2,8 +2,14 @@ package io.delilaheve.util
 
 import org.bukkit.ChatColor
 
+/**
+ * Colour utilities to ease [ChatColor] management
+ */
 object ColourUtil {
 
+    /**
+     * Colourise this [String] replacing chat colour codes
+     */
     fun String.colourise() = ChatColor.translateAlternateColorCodes(
         '&',
         this
